ab85f10431c2034b95f09a3c97c7926b7a0f80b1
zhujian <505169307@qq.com>
2019-05-28 14:32:33 +0800

    feat(layer): 添加softmax评分类实现

--- nn/layers.py
+++ nn/layers.py
@@ -234,3 +234,24 @@ class CrossEntropyLoss(object):
 
         grad_out[range(N), self.labels] -= 1
         return grad_out
+
+
+class Softmax(object):
+    """
+    softmax评分
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, scores):
+        return self.forward(scores)
+
+    def forward(self, scores):
+        # scores.shape == [N, C]
+        assert len(scores.shape) == 2
+        scores -= np.max(scores, axis=1, keepdims=True)
+        expscores = np.exp(scores)
+        probs = expscores / np.sum(expscores, axis=1, keepdims=True)
+
+        return probs
